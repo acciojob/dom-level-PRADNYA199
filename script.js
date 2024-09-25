36d9e7e27e765098fbfc3fa53103b9477e183445
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-25 22:16:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function getDOMLevel(){
+	let element =document.getElementById("level");
+	let level=0;
+	while(element)
+		{
+			level++;
+			element=element.parentElement;
+		}
+	alert("the level of the element is: n")
+}
+getDOMLevel()
